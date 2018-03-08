d23cae19ee6d135799312167971f5f1a87d90dfc
Bicky23 <Alex19427@gmail.com>
2018-03-08 13:00:29 +0530

    removed inconsistent tabs

--- q01_create_class/build.py
+++ q01_create_class/build.py
@@ -4,3 +4,12 @@ import math
 
 "write your solution here"
 
+
+class complex_numbers:
+    """The complex number class.
+
+    Attributes:
+    attr1 (x): Real part of complex number.
+    attr2 (y): Imaginary part of complex number.
+
+    """
--- q01_create_class/tests/test_complex_number.py
+++ q01_create_class/tests/test_complex_number.py
@@ -1,5 +1,5 @@
 import unittest
-from inspect import getargspec
+from inspect import getfullargspec
 from ..build import complex_numbers
 from greyatomlib.python_guided_project.q01_create_class.build import complex_number
 import pandas as pd
@@ -19,36 +19,36 @@ class Testing(unittest.TestCase):
 
     def test_initiate_function(self):
         self.assertEqual(a.real, c.real)
-	self.assertEqual(b.imag,d.imag)
+        self.assertEqual(b.imag,d.imag)
         
     def test_add(self):
-	x = a+b
-	y = c+d
-	self.assertEqual(x.real,y.real)
-	self.assertEqual(x.imag,y.imag)
+        x = a+b
+        y = c+d
+        self.assertEqual(x.real,y.real)
+        self.assertEqual(x.imag,y.imag)
 
     def test_sub(self):
-	x = a-b 
-	y = c-d
-	self.assertEqual(x.real,y.real)
-	self.assertEqual(x.imag,y.imag)
+        x = a-b
+        y = c-d
+        self.assertEqual(x.real,y.real)
+        self.assertEqual(x.imag,y.imag)
 
     def test_mul(self):
-	x = a*b 
-	y = c*d
-	self.assertEqual(x.real,y.real)
-	self.assertEqual(x.imag,y.imag)
+        x = a*b
+        y = c*d
+        self.assertEqual(x.real,y.real)
+        self.assertEqual(x.imag,y.imag)
    
     def test_div(self):
-	self.assertEqual(a.__truediv__(b),c.__truediv__(d))
+        self.assertEqual(a.__truediv__(b),c.__truediv__(d))
 
     def test_absolute_value(self):
-	self.assertEqual(a.abs(),c.abs())
+        self.assertEqual(a.abs(),c.abs())
 
     def test_argument(self):
-	self.assertEqual(a.argument(),c.argument())
+        self.assertEqual(a.argument(),c.argument())
 
     def test_conjugate(self):
-	self.assertEqual(a.real,c.real)
-	self.assertEqual(b.imag,d.imag)
+        self.assertEqual(a.real,c.real)
+        self.assertEqual(b.imag,d.imag)
 
